e3d23f51617191c0c5118a59de1af2e311e9a451
Kunj pratap singh <kunjpratapsingh790@gmail.com>
2024-02-25 22:52:10 +0530

    Counter it

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import { Counter } from './counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
